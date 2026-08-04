25f735f86522f9e90640d9ada2193181f5e7f26b
parroline <306466976+parroline@users.noreply.github.com>
2026-08-04 13:09:17 +0700

    refactor: simplify main function

--- main.cpp
+++ main.cpp
@@ -3,74 +3,84 @@
 #include <iomanip>
 #include <string>
 
+void showMenu(){
+    std::cout << std::string(50, '=') << std::endl;
+    std::cout << std::right << std::setw(33) << "STORE MANAGEMENT" << std::endl;
+    std::cout << std::string(50, '-') << std::endl;
+    std::cout << "1. Add Product" << std::endl;
+    std::cout << "2. Update Product" << std::endl;
+    std::cout << "3. Delete Product" << std::endl;
+    std::cout << "4. Search Product" << std::endl;
+    std::cout << "5. Show Products" << std::endl << std::endl;
+    std::cout << "6. Sell Product" << std::endl << std::endl;
+    std::cout << "7. Show Bills" << std::endl << std::endl;
+    std::cout << "8. Statistics" << std::endl << std::endl;
+    std::cout << "9. Clear All Products" << std::endl;
+    std::cout << "0. Exit" << std::endl;
+    std::cout << std::string(50, '-') << std::endl;
+        
+}
+
+void handleChoice(Store& store, int choice){
+    int id;
+    std::cout << std::endl;
+    switch (choice){
+        case 1:
+            store.addProduct();
+            break;
+        case 2:
+            std::cout << "ID: ";
+            std::cin >> id;
+
+            store.updateProduct(id);
+            break;
+        case 3:
+            std::cout << "ID: ";
+            std::cin >> id;
+
+            store.deleteProduct(id);
+            break;
+        case 4:
+            std::cout << "ID: ";
+            std::cin >> id;
+
+            store.searchProduct(id);
+            break;
+        case 5:
+            std::cout << std::endl;
+            store.showProducts();
+            break;
+        case 6:
+            store.sellProduct();
+            break;
+        case 7:
+            store.showAllBills();
+            break;
+        case 8:
+            break;
+        case 9:
+            store.clearProducts();
+            break;
+        case 0:
+            std::cout << "Exited." << std::endl;
+            break;
+        default:
+            std::cout << "Invalid Choice." << std::endl;
+    }
+}
+
 int main(){
     Store store;
 
     int choice;
 
     do{
-        std::cout << std::string(50, '=') << std::endl;
-        std::cout << std::right << std::setw(33) << "STORE MANAGEMENT" << std::endl;
-        std::cout << std::string(50, '-') << std::endl;
-        std::cout << "1. Add Product" << std::endl;
-        std::cout << "2. Update Product" << std::endl;
-        std::cout << "3. Delete Product" << std::endl;
-        std::cout << "4. Search Product" << std::endl;
-        std::cout << "5. Show Products" << std::endl << std::endl;
-        std::cout << "6. Sell Product" << std::endl << std::endl;
-        std::cout << "7. Show Bills" << std::endl << std::endl;
-        std::cout << "8. Statistics" << std::endl << std::endl;
-        std::cout << "9. Clear All Products" << std::endl;
-        std::cout << "0. Exit" << std::endl;
-        std::cout << std::string(50, '-') << std::endl;
-        
+        showMenu();
+
         std::cout << "Choose: ";
         std::cin >> choice;
-        int id;
-        std::cout << std::endl;
-        switch (choice){
-            case 1:
-                store.addProduct();
-                break;
-            case 2:
-                std::cout << "ID: ";
-                std::cin >> id;
+        handleChoice(store, choice);
 
-                store.updateProduct(id);
-                break;
-            case 3:
-                std::cout << "ID: ";
-                std::cin >> id;
-
-                store.deleteProduct(id);
-                break;
-            case 4:
-                std::cout << "ID: ";
-                std::cin >> id;
-
-                store.searchProduct(id);
-                break;
-            case 5:
-                std::cout << std::endl;
-                store.showProducts();
-                break;
-            case 6:
-                store.sellProduct();
-                break;
-            case 7:
-                store.showAllBills();
-                break;
-            case 8:
-                break;
-            case 9:
-                store.clearProducts();
-                break;
-            case 0:
-                std::cout << "Exited." << std::endl;
-                break;
-            default:
-                std::cout << "Invalid Choice." << std::endl;
-        }
         std::cout << std::string(50, '=') << std::endl << std::endl;
     } while(choice != 0);
 }
